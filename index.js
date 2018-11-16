5052c59655f7f81563e9c90afde1d6e9a93c2cce
bzx1412060644 <1412060644@qq.com>
2018-11-16 20:44:00 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+function dig(obj, target) {
+    function obj(target) {
+        while(this.target===obj.target)
+        {return this.target()}
+    }
+}
+var data = {
+  level1: {
+    level2: {
+      level3: 'some data'
+    }
+  }
+}
